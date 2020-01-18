1e16e1eb4d3e6be606318c813ab24258b5aca83a
Jazzy <jnixon228@west-mec.org>
2020-01-18 11:43:11 -0700

    below zero fix

--- scroll.js
+++ scroll.js
@@ -1,10 +1,17 @@
 'use strict';
 
+var count = parseInt(document.getElementById("value" + number).textContent, 10);
 
 function minusFrom(number) {
-    var count = parseInt(document.getElementById("value" + number).textContent, 10) * 1;
-    count -= 1;
-    document.getElementById("value"+number).textContent = count;
+    if (parseInt(document.getElementById("value" + number).textContent, 10) > 0) {
+        var count = parseInt(document.getElementById("value" + number).textContent, 10) * 1;
+        count -= 1;
+        document.getElementById("value"+number).textContent = count;
+    }
+    else {
+
+    }
+    
 }
 function addTo(number) {
     var count = parseInt(document.getElementById("value" + number).textContent, 10) *1;
